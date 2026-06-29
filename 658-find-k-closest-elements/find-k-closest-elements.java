1dc654d176c33c52e245f3b6c6f8ce0a229c6757
Diwakar Shaw <diwakarshaw288@gmail.com>
2026-06-30 03:24:38 +0530

    Time: 44 ms (19.24%) | Memory: 48 MB (23.63%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,30 +1,33 @@
+import java.util.*;
+
 class Solution {
-    class Pair implements Comparable<Pair>{
-        int ele;
-        int diff;
-        Pair(int ele,int diff){
-            this.ele=ele;
-            this.diff=diff;
-        }
-        public int compareTo(Pair p){
-            if(this.diff==p.diff) return this.ele-p.ele;
-            return this.diff-p.diff;
-        }
-    }
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        PriorityQueue<Pair>pq=new PriorityQueue<>(Collections.reverseOrder());
-        for(int val:arr){
-            int diff=Math.abs(x-val);
-            pq.add(new Pair(val,diff));
-            if(pq.size()>k) pq.poll();
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> {
+                if (a[1] == b[1])
+                    return b[0] - a[0];   // larger element first
+                return b[1] - a[1];       // larger difference first
+            }
+        );
+
+        for (int val : arr) {
+            int diff = Math.abs(val - x);
+
+            pq.offer(new int[]{val, diff});
+
+            if (pq.size() > k)
+                pq.poll();
         }
-        List<Integer> ans=new ArrayList();
-        while(pq.size()>0){
-            Pair p=pq.poll();
-            ans.add(p.ele);
+
+        List<Integer> ans = new ArrayList<>();
+
+        while (!pq.isEmpty()) {
+            ans.add(pq.poll()[0]);
         }
+
         Collections.sort(ans);
+
         return ans;
-        
     }
 }
