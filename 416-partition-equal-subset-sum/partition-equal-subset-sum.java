fca31d108452bf57afd880121e3cd8f8d16ebc85
Diwakar Shaw <diwakarshaw288@gmail.com>
2026-07-17 22:34:05 +0530

    Time: 97 ms (18.43%) | Memory: 131.3 MB (5.06%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,20 +1,34 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum = 0;
-        for (int num : nums) sum += num;
-
-        if (sum % 2 != 0) return false;
-
-        int target = sum / 2;
-        boolean[] dp = new boolean[target + 1];
-        dp[0] = true;
-
-        for (int num : nums) {
-            for (int s = target; s >= num; s--) {
-                dp[s] = dp[s] || dp[s - num];
-            }
+        
+        int n=nums.length;
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum+=nums[i];
         }
-
-        return dp[target];
+        Boolean[][] dp=new Boolean[n+1][sum+1];
+        if(sum%2!=0){
+            return false;
+        }else{
+            return solve(nums,n-1, (sum/2),dp);
+        }
+        
     }
+     static boolean solve(int[] nums, int n, int sum,Boolean[][] dp){
+        if(sum==0){
+            return true;
+        }
+        if(n==0){
+            return nums[0]==sum;
+        }
+        if(dp[n][sum]!=null){
+            return dp[n][sum];
+        }
+        boolean skip=solve(nums,n-1,sum,dp);
+        boolean take=false;
+        if(nums[n]<=sum){
+            take=solve(nums,n-1,sum-nums[n],dp);
+        }
+        return dp[n][sum]=skip || take;
+     }
 }
